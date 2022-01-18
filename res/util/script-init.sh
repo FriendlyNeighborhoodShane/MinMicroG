dd25765cb6ecd18e67427b504819280b298324c4
FriendlyNeighborhoodShane <shane.880088.supw@gmail.com>
2022-01-18 20:05:22 +0530

    npem: install microG APKs as user app
    The init script was used to do it before, but init.d has been all but
    abandoned by ROMs universally, so effectively the only situation in which
    the init script runs is for Magisk.
    
    This change will be very helpful for non-Magisk users, as they will no
    longer have to extract and install the APK by hand.
    
    Also deduplicate code from init script and make it just call npem.

--- res/util/script-init.sh
+++ res/util/script-init.sh
@@ -4,9 +4,5 @@
 # Wait for bootup
 while true; do [ "$(getprop sys.boot_completed)" = "1" ] && break; sleep 5; done;
 
-# Fix GMS permission troubles
-apk="/system/priv-app/MicroGGMSCore/MicroGGMSCore.apk";
-[ -f "$apk" ] && pm install -r "$apk";
-
 # Grant permissions
 npem;
